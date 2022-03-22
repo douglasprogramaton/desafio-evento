386933d1047b4696c37c3a47070e94339d8356f2
douglasprogramaton <douglassantosprogramaton@gmail.com>
2022-03-22 16:10:29 -0300

    att1

--- Program.cs
+++ Program.cs
@@ -30,30 +30,25 @@ namespace NOVO_EVENTO
             List<string> sorteio = new List<string>();
             List<int> Mesas = new List<int>();
 
-
-           // Random rdm = new Random();
-
             dynamic Items = new List<dynamic>();
             int repetir = 0;
 
             while (repetir < 5)
             {
-                // List<string> sorteio = new List<string>();
+
+                Console.WriteLine("\n CHEGADA NO EVENTO\n ");
+                
 
 
                 Console.Write("digite o nome do canditado:");
                 string nome = Console.ReadLine();
-                Console.Write($"escolha uma mesa de 1 há 20:" );
-                int Mesa = int.Parse(Console.ReadLine());
-
-
-                //int mesa = rdm.Next(1, 20);
-
+               
                 double consumo = 0.0;
                 string data = String.Format("{0:dd/MM/yyyy HH:mm:ss}", DateTime.Now);
                 Console.Write($"diga a idade: ");
                 int idade = int.Parse(Console.ReadLine());
-
+                
+                int Mesa = 0;
 
                 Items.Add(new { Nome = nome, Mesa = Mesa, Despesa = consumo, Time = data, Idade = idade });// serão add.nas listas criadas.
                 if (idade < 18)
@@ -63,7 +58,7 @@ namespace NOVO_EVENTO
 
                     Console.WriteLine("Menores de idade nao poderao participar mas tem acesso vip no playground");
 
-                    Console.WriteLine("$ Menor de idade ...[a]fliperamas | [b]futebol society | [c] ps5 ");
+                    Console.WriteLine("$ \n Menor de idade ...[a]fliperamas | [b]futebol society | [c] ps5 \n ");
 
                     char Ebrinquedo;
 
@@ -72,17 +67,17 @@ namespace NOVO_EVENTO
                     switch (Ebrinquedo)
                     {
                         case 'a':
-                            Console.WriteLine("esta jogando the king of fighters 95 a 98 | street fighter.");
+                            Console.WriteLine("\n esta jogando the king of fighters 95 a 98 | street fighter \n.");
                             break;
                         case 'b':
 
-                            Console.WriteLine(" estará jogando  futebol com o professor falcao.");
+                            Console.WriteLine("\n estará jogando  futebol com o professor falcao \n.");
                             break;
                         case 'c':
-                            Console.WriteLine(" estará jogando  COD WARZOONE .");
+                            Console.WriteLine("\n estará jogando  COD WARZOONE . \n");
                             break;
                         default:
-                            Console.WriteLine("opçao invalida");
+                            Console.WriteLine("\n opçao invalida \n");
                             break;
                     }
                 }
@@ -93,131 +88,112 @@ namespace NOVO_EVENTO
                 else
                 {
                     sorteio.Add(nome);
-                    Mesas.Add(Mesa);
+                    Console.Write($"escolha uma mesa de 1 há 20:");
+                    Mesas.Add(int.Parse(Console.ReadLine()));
                     
                 }
-
-
-                //Console.ReadKey();
-
                 string resultado = String.Empty;
-
-                //int candidato = 0;
                 char aceito;
 
-                Console.WriteLine("\n CHEGADA NO EVENTO ");
 
-                Console.WriteLine($"Deseja beber algo?:s/n");
+                Console.WriteLine($"........................Deseja beber algo?:s/n........................");
                 string drink = Console.ReadLine().ToLower();
-                if (drink == "s" && idade > 18)
+                if (drink == "s"  && idade > 18)
                 {
-
-                    Console.WriteLine("Escolh a bebida");
+                  
+                   
+                    Console.WriteLine(".......................Escolh a bebida.............................");
                     foreach (string b in Mbebidas)
                     {
                         Console.WriteLine("cardapio:{0}", b);
                     }
                     Console.ReadLine();
-                    Console.WriteLine("$ ESCOLHA A BEBIDA ...[a] COCA-COLA | [b] FANTA | [c] GUARANA | [d] CERVEJA | [e] WHISKY | [F] PINGA [g] agua...");
+                    Console.WriteLine("$       ESCOLHA A BEBIDA ...[a] COCA-COLA | [b] FANTA | [c] GUARANA | [d] CERVEJA | [e] WHISKY | [F] PINGA [g] agua...");
                     aceito = char.Parse(Console.ReadLine());
 
                     switch (aceito)
                     {
                         case 'a':
-                            Console.WriteLine("Servindo uma Coca-Cola bem gelada...");
+                            Console.WriteLine("\n Servindo uma Coca-Cola bem gelada...\n");
                             break;
                         case 'b':
-                            Console.WriteLine("Servindo uma Fanta bem gelada...");
+                            Console.WriteLine("\n Servindo uma Fanta bem gelada...\n");
                             break;
                         case 'c':
-                            Console.WriteLine("Servindo um Guarana bem gelado...");
+                            Console.WriteLine("\n Servindo um Guarana bem gelado... \n");
                             break;
                         case 'd':
-                            Console.WriteLine("Servindo uma Cerveja bem gelada...");
+                            Console.WriteLine("\n Servindo uma Cerveja bem gelada...\n");
                             break;
                         case 'e':
-                            Console.WriteLine(".....Whisky...melhor qualidade");
+                            Console.WriteLine("\n.....Whisky...melhor qualidade \n");
                             break;
                         case 'f':
-                            Console.WriteLine(".....Rabo de galo.....");
+                            Console.WriteLine("\n.....Rabo de galo.....\n");
                             break;
                         case 'g':
-                            Console.WriteLine(".....agua gelada.....");
+                            Console.WriteLine("\n.....agua gelada.....\n");
                             break;
                         default:
-                            Console.WriteLine("opçao invalido");
+                            Console.WriteLine("\n opçao invalido\n");
                             break;
 
                     }
-             //       Random rmd = new Random();
-               //     int tentativa = rdm.Next(1, 20);
-                 //   Console.ReadLine();
-                 //
-                    DateTime dataEntrada = DateTime.Now;
-                    Console.WriteLine(dataEntrada);
-
-                   // resultado = rdm.Next(1, 20).ToString();
-                    //Console.WriteLine(" Sentara na mesa:" + resultado);
-
+           
                 }
-
-
-                else if (idade < 18)
+                
+                else if (drink == "s" && idade < 18)
                 {
-                    Console.WriteLine("Escolh a bebida");
+                    Console.WriteLine("\n Escolh a bebida \n");
                     foreach (string bm in bebidasmenor)
                     {
                         Console.WriteLine("cardapio:{0}", bm);
                     }
                     Console.ReadLine();
-                    Console.WriteLine("$ ESCOLHA A BEBIDA ...[a] COCA-COLA | [b] FANTA | [c] GUARANA | [d] AGUA ");
+                    Console.WriteLine("$      ESCOLHA A BEBIDA ...[a] COCA-COLA | [b] FANTA | [c] GUARANA | [d] AGUA ");
                     aceito = char.Parse(Console.ReadLine());
                     switch (aceito)
                     {
                         case 'a':
-                            Console.WriteLine("Servindo uma Coca-Cola bem gelada...");
+                            Console.WriteLine("\n Servindo uma Coca-Cola bem gelada...\n");
                             break;
                         case 'b':
-                            Console.WriteLine("Servindo uma Fanta bem gelada...");
+                            Console.WriteLine("\n Servindo uma Fanta bem gelada...\n");
                             break;
                         case 'c':
-                            Console.WriteLine("Servindo um Guarana bem gelado...");
+                            Console.WriteLine("\n Servindo um Guarana bem gelado...\n");
                             break;
                         case 'd':
-                            Console.WriteLine("Servindo uma agua bem gelada...");
+                            Console.WriteLine("\n Servindo uma agua bem gelada...\n");
                             break;
                         default:
-                            Console.WriteLine("opçao invalido");
+                            Console.WriteLine("\n opçao invalido \n ");
                             break;
                     }
+                 
+
                 }
+                else
+                {
+                    drink = "n";
+                    Console.WriteLine(".......................OK estamos a disposição........................");
+                }
+                DateTime dataEntrada = DateTime.Now;
+                Console.WriteLine(dataEntrada);
                 repetir++;
                 if (repetir == 5)
                 {
-
 
                     string[] sorteados = new string[5];
                     sorteio.CopyTo(sorteados);
                     int[] MS = new int[5];
                     Mesas.CopyTo(MS);
-
-                    foreach (string s in sorteados)
-                    {
-
-                        //Console.WriteLine("vencedores do evento{0}",sorteados[0]);
-                        // Console.WriteLine("vencedores do evento:{0}", sorteados[1]);
-                        //Console.WriteLine("vencedores do evento:{0}", sorteados[2]);
-                    }
-
                     Console.WriteLine("primeira mesa sorteada:{0}",MS[0]);
                     Console.WriteLine("vencedores do evento:{0}", sorteados[0]);
                     Console.WriteLine("segunda mesa sorteada:{0}",MS[1]);
                     Console.WriteLine("vencedores do evento:{0}", sorteados[1]);
                     Console.WriteLine("terceira mesa sorteada:{0}",MS[2]);
                     Console.WriteLine("vencedores do evento:{0}", sorteados[2]);
-
-
-
 
                 }
             }
